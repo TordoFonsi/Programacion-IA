91e57a6a443b7a0f1fe8023210ec3e37d621b545
Toshiba <alfonsiluciano@gmail.com>
2026-09-14 21:17:24 -0300

    Estado de ataque añadido

--- Assets/Scripts/Hunter/FSMAgent.cs
+++ Assets/Scripts/Hunter/FSMAgent.cs
@@ -3,7 +3,8 @@ using UnityEngine;
 public enum PoliceStates
 {
     Idle,
-    Patrol
+    Patrol,
+    Attack
 }
 
 public class FSMAgent : MonoBehaviour
@@ -14,9 +15,20 @@ public class FSMAgent : MonoBehaviour
     [Header("Patrol")]
     [SerializeField] private PatrolData dataPatrol;
 
+    [Header("Attack")]
+    [SerializeField] private float tba = 3f;
+    [SerializeField] private float rangeAttackRadius = 10f;
+    [SerializeField] private float meleeAttackRadius = 2f;
+    [SerializeField] private int attackDamage = 25;
+
     private StateMachine stateMachine;
+    private float attackTimer;
 
     public float Speed => speed;
+    public float TBA => tba;
+    public float RangeAttackRadius => rangeAttackRadius;
+    public float MeleeAttackRadius => meleeAttackRadius;
+    public int AttackDamage => attackDamage;
 
     private void Awake()
     {
@@ -31,6 +43,11 @@ public class FSMAgent : MonoBehaviour
                 dataPatrol,
                 stateMachine);
 
+        AttackState attackState =
+            new AttackState(
+                this,
+                stateMachine);
+
         stateMachine.RegisterState(
             PoliceStates.Idle,
             idleState);
@@ -39,12 +56,28 @@ public class FSMAgent : MonoBehaviour
             PoliceStates.Patrol,
             patrolState);
 
+        stateMachine.RegisterState(
+            PoliceStates.Attack,
+            attackState);
+
         stateMachine.ChangeState(
             PoliceStates.Idle);
     }
 
     private void Update()
     {
+        attackTimer += Time.deltaTime;
+
         stateMachine.Update();
+    }
+
+    public bool CanAttack()
+    {
+        return attackTimer >= tba;
+    }
+
+    public void ResetAttackTimer()
+    {
+        attackTimer = 0f;
     }
 }
